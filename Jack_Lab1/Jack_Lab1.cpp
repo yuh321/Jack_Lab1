ea2d72c5778569f55f76d4416e86acb1446bfc59
Aja Reynolds <braylen334@gmail.com>
2026-04-15 15:06:28 -0500

    Added grade check feature

--- Jack_Lab1/Jack_Lab1.cpp
+++ Jack_Lab1/Jack_Lab1.cpp
@@ -5,8 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello from student A!" << std::endl;
-
+    std::cout << "Hello  from student B" << std::endl;
     std::cout << "Hello from Jack! This is my first Git commit" << std::endl;
     std::cout << "Git makes collaboration easy!" << std::endl;
 
@@ -14,5 +13,8 @@ int main()
     std::cout << "Lab 1 completed on [today's date]!" << std::endl;
 
     std::cout << "Feature by Jack: Branching is awesome!" << std::endl;
+    std::cout << "Change to the code ";
 
+    std::cout << "Student ID: [Your ID] | Name: [Your Name]" << std::endl;
+    std::cout << "Grade: [Your grade]" << std::endl;
 }
